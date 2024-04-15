4ee885ba27f06c57b23c0123cfd3e7e51818b887
icaris lab <165938682+icarislab@users.noreply.github.com>
2024-04-15 09:51:34 -0600

    Update to 32-bit-galois-prng_python_simulation.py

--- docs/32-bit-fibonacci-prng_python_simulation.py
+++ docs/32-bit-fibonacci-prng_python_simulation.py
@@ -5,7 +5,7 @@ Spyder Editor
 This is a temporary script file.
 """
 
-## 32-bit fibonacci prng
+## 32-bit galois prng
 
 import numpy as np
 
@@ -16,14 +16,18 @@ lfsr_reg = np.zeros(32, dtype=bool)
 for clocks in np.arange(0,100):
     
     print("Clock ", clocks,": ",lfsr_reg*1)
+    #print(lfsr_reg[31]*1)
     
     tmp_reg = lfsr_reg.copy()
     
     for count in np.arange(1,32):
         lfsr_reg[count] = tmp_reg[count-1]
-    
-    lfsr_reg[0] = tmp_reg[31] ^ tmp_reg[29] ^ tmp_reg[25] ^ tmp_reg[24]
-    
+        
+        lfsr_reg[0] = tmp_reg[31]
+        lfsr_reg[2] = tmp_reg[31] ^ tmp_reg[1]
+        lfsr_reg[6] = tmp_reg[31] ^ tmp_reg[5]
+        lfsr_reg[7] = tmp_reg[31] ^ tmp_reg[6]
+            
     
     if all(flag == 0 for (flag) in tmp_reg):
         lfsr_reg[0] = 1
